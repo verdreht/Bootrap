7bc0ce877c855a4e4daafc3f2bddf392505e91c9
verdreht <spieler.ramon@sus.htl-grieskirchen.at>
2021-07-19 17:46:55 +0200

    v0.2.1 Updated demo.js for github pages.

--- demo.js
+++ demo.js
@@ -1,14 +1,17 @@
-document.getElementById("button").onclick = function () {
-    let result = validateAll();
-    let btn = document.getElementById("button");
 
-    setTimeout(function () {
-        if(result) {
-            btn.innerHTML = "<i class=\"bi bi-check\"></i> Success";
-        } else {
-            btn.innerHTML = "Absenden";
-        }
-    }, 500)
+window.onload = function () {
+    document.getElementById("button").onclick = function () {
+        let result = validateAll();
+        let btn = document.getElementById("button");
 
+        setTimeout(function () {
+            if(result) {
+                btn.innerHTML = "<i class=\"bi bi-check\"></i> Success";
+            } else {
+                btn.innerHTML = "Absenden";
+            }
+        }, 500)
 
+    }
 }
+
