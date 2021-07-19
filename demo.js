267fd6b6ff41f5a5953a14655f740f655de68a93
verdreht <spieler.ramon@sus.htl-grieskirchen.at>
2021-07-19 16:55:26 +0200

    v0.2 Added form validation. The application can now validate given a number range in an input field, mail address and password field length.

---
+++ demo.js
@@ -0,0 +1,14 @@
+document.getElementById("button").onclick = function () {
+    let result = validateAll();
+    let btn = document.getElementById("button");
+
+    setTimeout(function () {
+        if(result) {
+            btn.innerHTML = "<i class=\"bi bi-check\"></i> Success";
+        } else {
+            btn.innerHTML = "Absenden";
+        }
+    }, 500)
+
+
+}
